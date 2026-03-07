696fcf2d524818be6a083d546e99d84149734caf
Rahul <rahulyt2006@gmail.com>
2026-03-07 21:54:45 +0530

    Fixed all Errors, Best Running APP

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`,
-  timeout: 10000,
+  timeout: 100000,
 });
 
 // Interceptor for consistent error handling
